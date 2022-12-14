9ebe8dd699d6c3930035a2febcfa484e2cb3aff2
ttabelhaxd <120480217+ttabelhaxd@users.noreply.github.com>
2022-12-14 00:05:50 +0000

    a

---
+++ competitionDetails.js
@@ -0,0 +1,96 @@
+// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    //---Variáveis locais
+    var self = this;
+    self.baseUri = ko.observable('http://192.168.160.58/Olympics/api/competitions/');
+    self.displayName = 'Competition Details';
+    self.error = ko.observable('');
+    self.passingMessage = ko.observable('');
+   //--- Data Record
+   self.Id = ko.observable('');
+   self.Modality = ko.observable('');
+   self.Name = ko.observable('');
+   self.Photo = ko.observable('');
+   self.Participant = ko.observableArray([]);
+   self.Url = ko.observable('');
+
+   //--- Page Events
+   self.activate = function (id) {
+       console.log('CALL: getCompetition...');
+       var composedUri = self.baseUri() + id;
+       ajaxHelper(composedUri, 'GET').done(function (data) {
+           console.log(data);
+           hideLoading();
+           self.Id(data.Id);
+           self.Modality(data.Modality);
+           self.Name(data.Name);
+           self.Photo(data.Photo);
+           self.Participant(data.Participant);
+       });
+   };
+
+   //--- Internal functions
+   function ajaxHelper(uri, method, data) {
+       self.error(''); // Clear error message
+       return $.ajax({
+           type: method,
+           url: uri,
+           dataType: 'json',
+           contentType: 'application/json',
+           data: data ? JSON.stringify(data) : null,
+           error: function (jqXHR, textStatus, errorThrown) {
+               console.log("AJAX Call[" + uri + "] Fail...");
+               hideLoading();
+               self.error(errorThrown);
+           }
+       });
+   }
+
+   function showLoading() {
+       $('#myModal').modal('show', {
+           backdrop: 'static',
+           keyboard: false
+       });
+   }
+   function hideLoading() {
+       $('#myModal').on('shown.bs.modal', function (e) {
+           $("#myModal").modal('hide');
+       })
+   }
+
+   function getUrlParameter(sParam) {
+       var sPageURL = window.location.search.substring(1),
+           sURLVariables = sPageURL.split('&'),
+           sParameterName,
+           i;
+
+       for (i = 0; i < sURLVariables.length; i++) {
+           sParameterName = sURLVariables[i].split('=');
+
+           if (sParameterName[0] === sParam) {
+               return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+           }
+       }
+   };
+
+   //--- start ....
+   showLoading();
+   var pg = getUrlParameter('id');
+   console.log(pg);
+   if (pg == undefined)
+       self.activate(1);
+   else {
+       self.activate(pg);
+   }
+   console.log("VM initialized!");
+};
+
+$(document).ready(function () {
+   console.log("document.ready!");
+   ko.applyBindings(new vm());
+});
+
+$(document).ajaxComplete(function (event, xhr, options) {
+   $("#myModal").modal('hide');
+})
